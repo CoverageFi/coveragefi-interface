bf0d26e5f7d9af82d559685072e8f6c13f0ab6fa
Gurpreet <gs109690@gmail.com>
2024-10-12 19:48:41 +0530

    intial commit

---
+++ wagmi.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from '@wagmi/cli'
+import { actions, hardhat, foundry } from '@wagmi/cli/plugins'
+
+export default defineConfig({
+  out: 'src/abis.ts',
+  contracts: [],
+  plugins: [
+    actions(), 
+  ],
+})
